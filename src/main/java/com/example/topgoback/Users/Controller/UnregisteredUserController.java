e9b9827612a98148fcd608361064deacf9ede937
MarkoErdelji <erdeljimarko@gmail.com>
2022-12-08 18:27:35 +0100

    feat: Changed variable names in unregistered user DTO

--- src/main/java/com/example/topgoback/Users/Controller/UnregisteredUserController.java
+++ src/main/java/com/example/topgoback/Users/Controller/UnregisteredUserController.java
@@ -2,7 +2,6 @@ package com.example.topgoback.Users.Controller;
 
 import com.example.topgoback.Users.DTO.UnregisteredUserAssumptionDTO;
 import com.example.topgoback.Users.DTO.UnregisteredUserDTO;
-import com.example.topgoback.Users.Model.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,8 +13,8 @@ public class UnregisteredUserController {
     @PostMapping(consumes = "application/json")
     public ResponseEntity<UnregisteredUserAssumptionDTO> getAssumption(@RequestBody UnregisteredUserDTO unregisteredUserDTO) {
         UnregisteredUserAssumptionDTO unregisteredUserAssumptionDTO = new UnregisteredUserAssumptionDTO();
-        unregisteredUserAssumptionDTO.setAssumedPrice(1200);
-        unregisteredUserAssumptionDTO.setAssumedTimeInMinutes(15);
+        unregisteredUserAssumptionDTO.setEstimatedCost(450);
+        unregisteredUserAssumptionDTO.setEstimatedTimeInMinutes(10);
         return new ResponseEntity<>(unregisteredUserAssumptionDTO, HttpStatus.CREATED);
     }
 
