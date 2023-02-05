47e8bf3a337dba651dd1cd38d4e4ae26c3dfa8cf
MarkoErdelji <erdeljimarko@gmail.com>
2023-02-05 18:40:28 +0100

    feat: Added repository tests for driver and passenger

--- src/test/java/com/example/topgoback/Rides/Repository/RideRepositoryTestEmbedded.java
+++ src/test/java/com/example/topgoback/Rides/Repository/RideRepositoryTestEmbedded.java
@@ -30,19 +30,33 @@ public class RideRepositoryTestEmbedded {
 
 
     @Test
-    public void testFindByDriverOrPassengerAndStartBetween() {
-        int userId = 4;
+    public void testFindByDriverOrPassengerAndStartBetweenWithDriverId_correctData_returnsPageOfRides() {
+        int driverId = 4;
         LocalDateTime startDate = LocalDateTime.parse("2023-01-01T00:00:00");
         LocalDateTime endDate = LocalDateTime.parse("2023-01-02T00:00:00");
         Pageable pageable = PageRequest.of(0, 10);
 
-        Page<Ride> rides = rideRepository.findByDriverOrPassengerAndStartBetween(userId, startDate, endDate, pageable);
-        assertEquals(8, rides.getNumberOfElements());
+        Page<Ride> rides = rideRepository.findByDriverOrPassengerAndStartBetween(driverId, startDate, endDate, pageable);
+        assertThat(rides.getTotalElements()).isEqualTo(2);
+        assertThat(rides.getContent().get(0).getId()).isEqualTo(1);
     }
 
 
     @Test
-    void testFindByDriverOrPassengerAndStartBetweenWithInvalidUserId() {
+    public void testFindByDriverOrPassengerAndStartBetweenWithPassengerId_correctData_returnsPageOfRides() {
+        int passengerId = 21;
+        LocalDateTime startDate = LocalDateTime.parse("2023-01-01T00:00:00");
+        LocalDateTime endDate = LocalDateTime.parse("2023-01-02T00:00:00");
+        Pageable pageable = PageRequest.of(0, 10);
+
+        Page<Ride> rides = rideRepository.findByDriverOrPassengerAndStartBetween(passengerId, startDate, endDate, pageable);
+        assertThat(rides.getTotalElements()).isEqualTo(2);
+        assertThat(rides.getContent().get(0).getId()).isEqualTo(1);
+    }
+
+
+    @Test
+    void testFindByDriverOrPassengerAndStartBetween_incorrectUserId_returnsEmptyPage() {
         int invalidUserId = 0;
         LocalDateTime startDate = LocalDateTime.of(2023, 1, 1, 0, 0);
         LocalDateTime endDate = LocalDateTime.of(2023, 1, 2, 0, 0);
@@ -52,12 +66,87 @@ public class RideRepositoryTestEmbedded {
     }
 
     @Test
-    void testFindByDriverOrPassengerAndStartBetweenWithInvalidDateRange() {
+    void testFindByDriverOrPassengerAndStartBetween_incorrectDates_returnsEmptyPage() {
         int userId = 4;
         LocalDateTime startDate = LocalDateTime.of(2023, 1, 1, 0, 0);
         LocalDateTime endDate = LocalDateTime.of(2022, 1, 2, 0, 0);
         Pageable pageable = PageRequest.of(0, 10);
         Page<Ride> rides = rideRepository.findByDriverOrPassengerAndStartBetween(userId, startDate, endDate, pageable);
         assertTrue(rides.isEmpty());
+    }
+
+
+    @Test
+    public void testfindByPassengerAndBeginBetween_correctData_returnsPageOfRides() {
+        LocalDateTime startDate = LocalDateTime.parse("2023-01-01T08:00:00");
+        LocalDateTime endDate = LocalDateTime.parse("2023-01-01T12:00:00");
+        Pageable pageable = PageRequest.of(0, 10);
+
+        Page<Ride> rides = rideRepository.findByPassengerAndBeginBetween(21, startDate, endDate, pageable);
+
+        assertThat(rides.getTotalElements()).isEqualTo(1);
+        assertThat(rides.getContent().get(0).getId()).isEqualTo(1);
+    }
+
+    @Test
+    public void testfindByPassengerAndBeginBetween_incorrectUserId_returnsEmptyPage() {
+        LocalDateTime startDate = LocalDateTime.parse("2023-01-01T09:00:00");
+        LocalDateTime endDate = LocalDateTime.parse("2023-01-01T13:00:00");
+        Pageable pageable = PageRequest.of(0, 10);
+
+        Page<Ride> rides = rideRepository.findByPassengerAndBeginBetween(999, startDate, endDate, pageable);
+
+        assertThat(rides.getTotalElements()).isEqualTo(0);
+    }
+
+    @Test
+    public void testfindByPassengerAndBeginBetween_incorrectDates_returnsEmptyPage() {
+        LocalDateTime startDate = LocalDateTime.parse("2020-01-01T09:00:00");
+        LocalDateTime endDate = LocalDateTime.parse("2020-01-01T15:00:00");
+        Pageable pageable = PageRequest.of(0, 10);
+
+        Page<Ride> rides = rideRepository.findByPassengerAndBeginBetween(999, startDate, endDate, pageable);
+
+        assertThat(rides.getTotalElements()).isEqualTo(0);
+    }
+
+    @Test
+    public void testfindByDriverAndBeginBetweenTest_correctData_returnsPageOfRides() {
+        int userId = 4;
+        LocalDateTime startDate = LocalDateTime.of(2023, 1, 1, 8, 0, 0);
+        LocalDateTime endDate = LocalDateTime.of(2023, 1, 1, 19, 0, 0);
+        Pageable pageable = PageRequest.of(0, 10);
+
+        Page<Ride> rides = rideRepository.findByDriverAndBeginBetween(userId, startDate, endDate, pageable);
+        assertEquals(2, rides.getTotalElements());
+
+        List<Ride> rideList = rides.getContent();
+        assertEquals(2, rideList.size());
+        assertEquals(1, rideList.get(0).getId());
+        assertEquals(2, rideList.get(1).getId());
+    }
+
+    @Test
+    public void testFindByDriverAndBeginBetween_incorrectUserId_returnsEmptyPage() {
+        int userId = 21;
+        LocalDateTime startDate = LocalDateTime.parse("2023-01-01T09:00:00");
+        LocalDateTime endDate = LocalDateTime.parse("2023-01-01T15:00:00");
+        Pageable pageable = PageRequest.of(0, 10);
+
+        Page<Ride> rides = rideRepository.findByDriverAndBeginBetween(userId, startDate, endDate, pageable);
+
+        assertEquals(0, rides.getTotalElements());
+    }
+
+    @Test
+    public void testFindByDriverAndBeginBetween_incorrectDates_returnsEmptyPage() {
+        int userId = 4;
+        LocalDateTime startDate = LocalDateTime.parse("2020-01-01T09:00:00");
+        LocalDateTime endDate = LocalDateTime.parse("2020-01-01T15:00:00");
+        Pageable pageable = PageRequest.of(0, 10);
+
+        Page<Ride> rides = rideRepository.findByDriverAndBeginBetween(userId, startDate, endDate, pageable);
+
+        assertEquals(0, rides.getTotalElements());
     }
 }
